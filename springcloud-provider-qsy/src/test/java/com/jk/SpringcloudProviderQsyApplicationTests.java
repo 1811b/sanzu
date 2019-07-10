0d978b7f1720fe91a92ce76e7bc42c67dd619431
qishengyun <2912428101@qq.com>
2019-07-10 10:55:54 +0800

    生产

--- springcloud-provider-qsy/src/test/java/com/jk/SpringcloudProviderQsyApplicationTests.java
+++ springcloud-provider-qsy/src/test/java/com/jk/SpringcloudProviderQsyApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookCkConsumerApplicationTests {
+public class SpringcloudProviderQsyApplicationTests {
 
     @Test
     public void contextLoads() {
