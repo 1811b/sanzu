550944f9b48899c52399c9dfef1d019e0e62ab15
账guoxu98· <2775236224@qq.com>
2019-07-10 11:51:56 +0800

    gx

---
+++ guo-xiaofei/src/main/java/com/jk/GuoXiaofeiApplication.java
@@ -0,0 +1,22 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GuoXiaofeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuoXiaofeiApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
