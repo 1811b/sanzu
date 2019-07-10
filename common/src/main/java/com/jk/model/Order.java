4b94dd6d1c8c7e4c4f0f5011c24a4563efd51d6d
chenkaig <1325453678@qq.com>
2019-07-10 10:46:57 +0800

    order表修正

--- common/src/main/java/com/jk/model/Order.java
+++ common/src/main/java/com/jk/model/Order.java
@@ -12,20 +12,22 @@ import java.util.Date;
 public class Order implements Serializable {
     private Integer  orderId;//  --主键
     private String    orderNo;//  --订单号
-    private Integer   userId;//  --关联用户表
+    private Integer   userId ;// --谁买的
     private String     commodityNo;//  --商品编号
+    private String      proName ;//   --商品名称
     private Integer   orderStatus;//  --订单状态1、待评价2、已评价3、追加评价
-    private BigDecimal orderPrice;//  --订单价格
-    private Integer    areaId;//   ---关联收货地址表
+    private BigDecimal    sumPrice;//  --总价格
+    private String      userName;//    --收货人
     private String     areaName;//  --收货地址
-    private Integer   expressId;//  ---快递Id
     private String     expressName;//   --快递单号
     @DateTimeFormat(pattern="yyyy-MM-dd")
     @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
-    private Date createTime;//   --下单时间
+    private Date       createTime;//   --下单时间
     @DateTimeFormat(pattern="yyyy-MM-dd")
     @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private  Date      updateTime;//  --更新时间
     private String   payNo;//   --支付交易号--关联支付表
     private String   payText;//  --支付信息
+    private Integer   proNumber ;// --商品数量
+    private BigDecimal    orderPrice;//  --商品价格
 }
